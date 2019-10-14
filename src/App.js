914a5d2da30a53334726e36ed77e582c1513d0cf
marwinburesch <marwin@posteo.de>
2019-10-14 13:10:30 +0200

    add Skulls and a Button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Head from "./components/Head";
+import styled from "styled-components";
+import Button from "./components/Button";
+
+const Grid = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid>
+        <Head />
+        <Head />
+        <Head />
+        <Head />
+      </Grid>
+      {/* <Button size={"large"} /> */}
     </div>
   );
 }
